23012e65f6c4e09dff4d6b83fdee95b3f5c926e3
Aylass <viingoflaas@gmail.com>
2020-10-03 16:57:18 -0300

    Telas iniciais

---
+++ Assets/Scripts/TeladeBusca.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeladeBusca : MonoBehaviour
+{
+    public InputField inputSenha;
+    public GameObject TelaPerfil;
+    public GameObject TelaBusca;
+    // Start is called before the first frame update
+    public void clicouBotao(){
+        //pegar as informações do iput(inputSenha.text) e achar o respecivo perfil
+        
+        Debug.Log(inputSenha.text);
+        TelaPerfil.SetActive(true);
+        TelaBusca.SetActive(false);
+    }
+
+}
